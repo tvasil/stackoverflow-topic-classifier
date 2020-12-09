d3ac5649f33ff6230835c83ebfddb30a50fea3f1
Tania Vasilikioti <vassilikiotis@hotmail.com>
2020-12-09 14:29:35 +0100

    rewrite prediction to return label and probability

--- prediction/so_tag_classifier_prediction/predict.py
+++ prediction/so_tag_classifier_prediction/predict.py
@@ -1,8 +1,8 @@
-# TODO: Add possiblity to read from S3
+# TODO: Add possiblity to read the model from S3
 import argparse
 
 import joblib
-from so_tag_classifier_core import text_prepare, tokenize_and_stem
+from so_tag_classifier_core import _TAGS_TO_KEEP, text_prepare, tokenize_and_stem
 
 model = None
 mlb = None
@@ -15,13 +15,27 @@ def prepare_model_if_needed(fname: str) -> None:
         model, mlb = joblib.load(fname)
 
 
-def predict(sentence: str, fname: str) -> list:
-    """ Makes a label prediction based on a sentence and a model"""
+def get_preds(model, binarizer, sentence: str) -> set:
+    """Get labels of prediction for a single sentence (hence the 0)"""
+    raw_preds = model.predict([sentence])
+    return binarizer.inverse_transform(raw_preds)[0]
+
+
+def get_probability_preds(model, sentence: str) -> dict:
+    """Get probabilities for all tags possible for a single sentence (hence the 0)"""
+    global _TAGS_TO_KEEP
+    probs = model.predict_proba([sentence])[0]
+    return dict(zip(_TAGS_TO_KEEP, probs))
+
+
+def predict(sentence: str, fname: str) -> dict:
+    """ Makes a label prediction with probability, based on a sentence and a model"""
     global model, mlb
     prepare_model_if_needed(fname)
 
-    pred = model.predict([sentence])
-    return mlb.inverse_transform(pred)
+    preds = get_preds(model, mlb, sentence)
+    probs_dict = get_probability_preds(model, sentence)
+    return dict(zip(preds, [probs_dict.get(key) for key in preds]))
 
 
 if __name__ == "__main__":
