c3e6ac4bbfa68472693723765042ccb0750eef2f
Tania Vasilikioti <vassilikiotis@hotmail.com>
2020-12-09 14:29:08 +0100

    export TAGS_TO_KEEP from core

--- core/so_tag_classifier_core/__init__.py
+++ core/so_tag_classifier_core/__init__.py
@@ -1,9 +1,10 @@
 from so_tag_classifier_core.evaluation_logs import log_performance
 from so_tag_classifier_core.preprocessing_steps import (
+    _TAGS_TO_KEEP,
     binarize_ys,
     text_prepare,
     tokenize_and_stem,
     transform_y,
 )
 
-__all__ = [log_performance, text_prepare, binarize_ys, tokenize_and_stem, transform_y]
+__all__ = [log_performance, text_prepare, binarize_ys, tokenize_and_stem, transform_y, _TAGS_TO_KEEP]
