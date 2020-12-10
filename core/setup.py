29f43b2cb330bb2e6a1d3e3baa930a800bc11f8a
Tania Vasilikioti <vassilikiotis@hotmail.com>
2020-12-10 22:56:42 +0100

    add yaml to requirements

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-install_requires = ["joblib>=0.17.0", "nltk>=3.5", "scikit-learn==0.23.2"]
+install_requires = ["joblib>=0.17.0", "nltk>=3.5", "scikit-learn==0.23.2", "pyyaml==5.3.1"]
 
 extras_require = {"dev": ["pytest", "pre-commit"]}
 
